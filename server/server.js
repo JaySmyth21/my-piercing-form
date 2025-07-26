410f707ab41c7ebc50d56e47b0df025eed2f39ca
Jason Smyth <jason.s.smyth@gmail.com>
2025-07-26 13:10:02 -0700

    removed client folder as vercel does not recognize it

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://your-frontend.vercel.app" }));
+
 
 
 // ✅ Convert formData into WaitWhile dataFields
